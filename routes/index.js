e494348e2db94491cb26ce9ce4937f59bcbb5dfa
haseeb-shoukat <haseebshoukat52@gmail.com>
2023-04-11 03:53:53 +0500

    Display log in and sign up pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+/* GET sign up page. */
+router.get("/sign-up", function (req, res, next) {
+  res.render("sign-up");
+});
+
+/* GET log in page. */
+router.get("/log-in", function (req, res, next) {
+  res.render("log-in");
 });
 
 module.exports = router;
